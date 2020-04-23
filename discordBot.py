2b9fa5097b66b45a9745b91d1e080e6fba8c6ce9
CJ <cjhernaez@me.com>
2020-04-22 21:42:58 -0400

    Initial Framework for Discord Bot

---
+++ discordBot.py
@@ -0,0 +1,29 @@
+import discord
+
+client = discord.Client()
+
+users = {}
+users['Jarren'] = 'procrastinise#2594'
+users['CJ'] = 'Prophecies#9660'
+users['EJ'] = 'AirWick#9172'
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if 'valarant' in message.content.lower():
+        if message.author.name in users['CJ']:
+            await message.channel.send('shotguns only')
+        if message.author.name in users['Jarren']:
+            await message.channel.send('do it for the clip? I told you to say hi to your mom for me you cuck')
+        if message.author.name in users['EJ']:
+            await message.channel.send('EJ please by the operator ur a liability.')
+    if message.author == client.user:
+        return
+
+    if ('hello') in message.content:
+        await message.channel.send('Hello, {0.author}!'.format(message))
+
+client.run('NzAyNjY5Mzk5ODEwNzY4OTA5.XqDlIA.uDi-4hzEeWjofbTOQ0f0tGZe9YE')
